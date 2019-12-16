d3fbc746937ee0f721140be69b47cc3ad92eda76
youngjunna <yjna4006@gmail.com>
2019-12-16 13:28:25 -0800

    Refactor data structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,109 @@
+let offense = [
+  {
+    "name": "sf19",
+    "parent": ""
+  },
 
-// const margin = { top: 40, right: 20, bottom: 50, left: 100 };
-// const graphWidth = 560 - margin.right - margin.left;
-// const graphHeight = 400 - margin.top - margin.bottom;
+  {
+    "name": "Quarterback",
+    "parent": "sf19"
+  },
+  {
+    "name": "Tackle",
+    "parent": "sf19"
+  },
+  {
+    "name": "Tight End",
+    "parent": "sf19"
+  },
+  {
+    "name": "Running Back",
+    "parent": "sf19"
+  },
+  {
+    "name": "Wide Receiver",
+    "parent": "sf19"
+  },
+  {
+    "name": "Guard",
+    "parent": "sf19"
+  },
+  {
+    "name": "Center",
+    "parent": "sf19"
+  }
+];
+let defense = [{
+  "name": "Defensive End",
+  "parent": "sf19"
+},
+  {
+    "name": "Defensive Tackle",
+    "parent": "sf19"
+  },
+  {
+    "name": "Cornerback",
+    "parent": "sf19"
+  },
+  {
+    "name": "Safety",
+    "parent": "sf19"
+  },
+  {
+    "name": "Linebacker",
+    "parent": "sf19"
+  }
+];
+
+let team = require('../data/sf19.json');
+let data = offense.concat(team.filter(d => d.side === "offense"))
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 1000)
+  .attr('height', 800);
+
+const graph = svg.append('g')
+  .attr('transform', 'translate(50, 50)');
+
+const stratify = d3.stratify()
+  .id(d => d.name)
+  .parentId(d => d.parent);
+
+const rootNode = stratify(data)
+  .sum(d => d.salary);
+
+// bubble pack gen
+
+const pack = d3.pack()
+  .size([960, 700])
+  .padding(5) //gap between circles
+
+const bubbleData = pack(rootNode).descendants();
 
 
-// const svg = d3.select('.canvas')
-//   .append('svg')
-//   .attr('width', 500)  // this is so modification of margin will affect all changes
-//   .attr('height', 600)
-//   .attr('fill', 'black');
+//create ordinal scale
+const color = d3.scaleOrdinal(['purple', 'lightpurple', 'pink'])
 
-// const circle = svg.append('circle')
-//   .attr('r', 300)
-//   .attr('cx', 50)
-//   .attr('fill', 'purple')
+//join data and add group for each node
+const nodes = graph.selectAll('g')
+  .data(bubbleData)
+  .enter()
+  .append('g')
+  .attr('transform', d => {
+    debugger;
+    `translate(${d.x}, ${d.y})`})
+
+nodes.append('circle')
+  .attr('r', d => d.r)
+  .attr('stroke', 'white')
+  .attr('stroke-width', 2)
+  .attr('fill', d => color(d.depth));
+
+nodes.filter(node => !node.children)
+  .append('text')
+  .attr('text-anchor', 'middle')
+  .attr('dy', '0.3em') // moves text down
+  .attr('fill', 'white')
+  // .style('font-size', d => d.value * 5)
+  .text(d => d.data.name)
+
--- src/test.js
+++ src/test.js
@@ -1,48 +1,69 @@
+
 const OFFENSE = ["QB", "LT", "RT", "C", "G", "WR", "RB", "FB", "T", "TE"];
 const DEFENSE = ["DE", "CB", "DT", "FS", "ILB", "LB", "OLB", "S", "SS"];
 const SPECIAL = ["LS", "K", "P"];
 const team = "sf19";
-d3.csv(`../data/${team}.csv`, (d) => {
-
+// d3.csv(`../data/${team}.csv`, (d) => {
+const jsonCreate = d3.csv(`../data/sf19.csv`, (d) => {
+  
   let name = d["Players"];
   let pos = d["POS."];
   let salary = d["CAP HIT"].substring(1);
   let side;
   let parent;
-
-  if (name.includes('\r')) name = name.substring(0, name.indexOf('\r'))
+  if (name.includes('\n')) name = name.substring(0, name.indexOf('\n'))
+ 
   if (OFFENSE.includes(pos)) side = "offense";
   if (DEFENSE.includes(pos)) side = "defense";
   if (SPECIAL.includes(pos)) side = "special";
   switch (pos) {
     case "QB":
+      parent = "Quarterback";
+      break;
     case "C":
+      parent = "Center"
+      break;
     case "G":
+      parent = "Guard"
+      break;
     case "WR":
+      parent = "Wide Receiver"
+      break;
     case "TE":
+      parent = "Tight End"
+      break;
     case "DE":
+      parent = "Defensive End"
+      break;
     case "CB":
+      parent = "Cornerback"
+      break;
     case "DT":
-      parent = pos;
+      parent = "Defensive Tackle"
       break;
     case "LT":
     case "RT":
     case "T":
-      parent = "T";
+      parent = "Tackle";
       break;
     case "RB":
     case "FB":
-      parent = "RB";
+      parent = "Running Back";
       break;
     case "ILB":
     case "OLB":
     case "LB":
-      parent = "LB";
+      parent = "Linebacker";
       break;
     case "S":
     case "FS":
     case "SS":
-      parent = "S"
+      parent = "Safety"
+      break;
+    case "K":
+    case "P":
+    case "LS":
+      parent = "Special Team"
       break;
   }
   return {
@@ -54,5 +75,14 @@ d3.csv(`../data/${team}.csv`, (d) => {
     parent
   }
 }).then( d=> {
-  debugger;
-})
+  console.log(d);
+});
+//   fs.writeFile('../data/sf19.json', JSON.stringify(d, null, 4), (error) => {
+//     if (err) {
+//       console.log(error);
+//       return;
+//     };
+//     console.log("file created!");
+//   }
+// )});
+
